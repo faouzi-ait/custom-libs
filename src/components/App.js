3fb8ce00dc7299659260bcbe320f16ddee449e64
Faouzi Aitelhara <faouzi.aitelhara@gmail.com>
2023-10-14 16:51:17 +0100

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '../styles/App.module.scss';
+
+const App = () => {
+  const test_message = "HELLOOOOOOOOOO";
+
+  return (
+    <div className={styles.app}>
+      <h1>{test_message}, React App!</h1>
+    </div>
+  );
+};
+
+export default App;
